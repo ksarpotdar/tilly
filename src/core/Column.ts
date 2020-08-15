5d6a8d51c8425a09431c9d966f848feaf7772300
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-08-15 07:57:08 +0100

    Tidy

--- src/core/Column.ts
+++ src/core/Column.ts
@@ -90,7 +90,7 @@ export class Column {
 	 * @param value The value to test against.
 	 * @returns Returns the predicate to be used within a query where method.
 	 */
-	equals(value: unknown): Predicate {
+	public equals(value: unknown): Predicate {
 		const position = this.values.indexOf(value);
 
 		return index => this.index[index] === position;
@@ -101,11 +101,11 @@ export class Column {
 	 * @param regex A regular expression that will be tested to select rows.
 	 * @returns Returns the predicate to be used within a query where method.
 	 */
-	like(regex: RegExp): Predicate {
-		const indicies = this.values.filter(value => regex.test(String(value))).map((value, index) => index);
+	public like(regex: RegExp): Predicate {
+		const indices = this.values.filter(value => regex.test(String(value))).map((value, index) => index);
 
 		// Return a function that returns true if the row matches the regex results
-		return index => indicies.indexOf(this.index[index]) !== -1;
+		return index => indices.indexOf(this.index[index]) !== -1;
 	}
 
 	/**
@@ -113,9 +113,9 @@ export class Column {
 	 * @param values A list of values to test the column against.
 	 * @returns Returns the predicate to be used within a query where method.
 	 */
-	list(...values: unknown[]): Predicate {
-		const indicies = this.values.filter(value => values.indexOf(value) !== -1).map((value, index) => index);
+	public list(...values: unknown[]): Predicate {
+		const indices = this.values.filter(value => values.indexOf(value) !== -1).map((value, index) => index);
 
-		return index => indicies.indexOf(this.index[index]) !== -1;
+		return index => indices.indexOf(this.index[index]) !== -1;
 	}
 }
